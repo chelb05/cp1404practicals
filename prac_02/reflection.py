8699290971d8fc397bce09e270986dfc6e53c8cc
chelb05 <chelsea.bath@my.jcu.edu.au>
2025-10-09 13:47:32 +1000

    Write an overall reflection on this subject so far

---
+++ prac_02/reflection.py
@@ -0,0 +1,27 @@
+# CP1404 Practical Reflection
+
+## What percentage of the lecture videos have you watched so far?
+
+# Unsure
+
+## What time, location and duration have you scheduled to watch lecture videos?
+
+# after I finish work, approximately 11:30 pm
+
+## What are you doing well in the subject so far that you will keep doing?
+
+#
+
+## What do you need to stop or change in your work for this subject overall so far?
+
+#
+
+## What are you currently doing poorly for practicals in this subject that you will change?
+
+# I need to read the prac instructions more clearly and work out a better and
+# more do-able schedule to complete them. I need to better understand different
+# terms by potentially doing a list up of different words that I can understand
+
+## What are you doing really well for practicals in this subject?
+
+# I believe I am doing well with the actual tasks, and working through how to do them
