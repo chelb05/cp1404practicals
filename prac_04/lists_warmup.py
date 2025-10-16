c8a11c6a648bcfc0d91371f534584fa3a36af18e
chelb05 <chelsea.bath@my.jcu.edu.au>
2025-10-16 13:08:18 +1000

    Lists.

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,34 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+print(numbers[0])
+print(numbers[-1])
+print(numbers[3])
+print(numbers[:-1])
+print(numbers[3:4])
+print(5 in numbers)
+print(7 in numbers)
+print("3" in numbers)
+print(numbers + [6, 5, 3])
+
+# Results:
+# 3
+# 2
+# 1
+# [3, 1, 4, 1, 5, 9]
+# [1]
+# True
+# False
+# False
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# a
+numbers[0] = "ten" # Change first element of numbers to string.
+
+# b
+numbers[-1] = 1 # Changing last element in numbers to 1.
+
+# c
+print(numbers[2:]) # Print all except the first two.
+
+# d
+print(9 in numbers) # Whether 9 is in the numbers.
