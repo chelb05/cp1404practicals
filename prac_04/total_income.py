799572c4738cd39978439288f2f9392dcda82996
chelb05 <chelsea.bath@my.jcu.edu.au>
2025-10-16 13:06:18 +1000

    Refactor the naming of two variables, and create a seperate function for printing the List of Incomes.

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -4,21 +4,22 @@ Starter code for cumulative total income program
 """
 
 
-def main():
-    """Display income report for incomes over a given number of months."""
-    incomes = []
-    months = int(input("How many months? "))
-
-    for month in range(1, months + 1):
-        income = int(input(f"Enter income for month {month}: "))
-        incomes.append(income)
-
+def print_income_report(incomes, number_of_months):  # Create a new function for printing the report.
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
+def main():
+    list_of_incomes = []  # Fix the naming for incomes and income.
+    number_of_months = int(input("How many months? ")) # Fix naming for month and months.
+
+    for month in range(1, number_of_months + 1):
+        income = int(input(f"Enter income for month {month}: "))
+        list_of_incomes.append(income)
+
+    print_income_report(list_of_incomes, number_of_months)
 
 main()
